88bc0d42b7cbf1b1d859c2b7456a90b27beda93a
Bernardo Santos <bernardo.santos10@gmail.com>
2018-10-17 11:46:21 +0100

    Camera working, materials modified

--- tp1/primitives/MyCylinder.js
+++ tp1/primitives/MyCylinder.js
@@ -1,70 +1,80 @@
-class MyCylinder{ 
-    constructor(scene, height,  base, top, stacks, slices)
-    {
-        CGFobject.call(this, scene);
+function MyCylinder(scene, base, top, height, slices, stacks) {
+    CGFobject.call(this, scene);
 
-        this.slices = slices;
-        this.stacks = stacks;
-        this.base = base;
-        this.top = top;
-        this.height = height;
+    this.slices = parseInt(slices);
+    this.stacks = parseInt(stacks);
+    this.base = parseFloat(base);
+    this.top = parseFloat(top);
+    this.height = parseFloat(height);
 
-        this.deltaHeight = this.height / this.stacks;
-        this.delta = (this.top - this.base) / this.stacks;
+    this.deltaHeight = this.height / this.stacks;
+    this.delta = (this.top - this.base) / this.stacks;
 
-        this.initBuffers();
-}
-    initBuffers() {
+    this.initBuffers();
+};
 
-        var stacks = this.stacks;
+MyCylinder.prototype = Object.create(CGFobject.prototype);
+MyCylinder.prototype.constructor = MyCylinder;
 
-        var n = -2 * Math.PI / this.slices;
+MyCylinder.prototype.initBuffers = function() {
 
-        this.vertices = [];
-        this.normals = [];
-        this.indices = [];
-        this.texCoords = [];
+    //Sets the number os sides;
+
+    var stacks = this.stacks;
+
+    var n = -2 * Math.PI / this.slices;
+
+    this.vertices = [];
+    this.normals = [];
+    this.indices = [];
+    this.texCoords = [];
 
 
-        var patchLengthx = 1 / this.slices;
-        var patchLengthy = 1 / this.stacks;
-        var xCoord = 0;
-        var yCoord = 0;
+    var patchLengthx = 1 / this.slices;
+    var patchLengthy = 1 / this.stacks;
+    var xCoord = 0;
+    var yCoord = 0;
 
-        for (var q = 0; q < this.stacks+1 ; q++) {
+    for (var q = 0; q < this.stacks+1 ; q++) {
 
-            var z = (q * this.deltaHeight / this.stacks);
-            var inc = (q * this.delta) + this.base;
+        var z = (q * this.deltaHeight / this.stacks);
+        var inc = (q * this.delta) + this.base;
 
-            for (var i = 0; i <= this.slices; i++) {
 
-                this.vertices.push(inc * Math.cos(i * n), inc * Math.sin(i * n), q * this.deltaHeight);
-                this.normals.push(Math.cos(i * n), Math.sin(i * n), 0);
 
-                this.texCoords.push(xCoord, yCoord);
+        for (var i = 0; i <= this.slices; i++) {
 
-                xCoord += patchLengthx;
 
-            }
+            this.vertices.push(inc * Math.cos(i * n), inc * Math.sin(i * n), q * this.deltaHeight);
+            this.normals.push(Math.cos(i * n), Math.sin(i * n), 0);
 
-            xCoord = 0;
-            yCoord += patchLengthy;
+            this.texCoords.push(xCoord, yCoord);
+
+
+            xCoord += patchLengthx;
+
 
         }
 
+        xCoord = 0;
+        yCoord += patchLengthy;
 
-        var sides = this.slices +1;
+    }
+	
 
-        for (var q = 0; q < this.stacks; q++) {
-            for (var i = 0; i < this.slices; i++) {
+	var sides = this.slices +1;
 
-                this.indices.push(sides*q+i, sides*(q+1)+i, sides*q+i+1);
-                this.indices.push(sides*q+i+1, sides*(q+1)+i, sides*(q+1)+i+1);
 
-                this.indices.push(sides*q+i, sides*q+i+1, sides*(q+1)+i);
-                this.indices.push(sides*q+i+1, sides*(q+1)+i+1, sides*(q+1)+i);
+    for (var q = 0; q < this.stacks; q++) {
+        for (var i = 0; i < this.slices; i++) {
 
-            }
+			this.indices.push(sides*q+i, sides*(q+1)+i, sides*q+i+1);
+			this.indices.push(sides*q+i+1, sides*(q+1)+i, sides*(q+1)+i+1);
+
+			this.indices.push(sides*q+i, sides*q+i+1, sides*(q+1)+i);
+			this.indices.push(sides*q+i+1, sides*(q+1)+i+1, sides*(q+1)+i);
+
+        }
 
     }
 
@@ -74,5 +84,4 @@ class MyCylinder{
     this.primitiveType = this.scene.gl.TRIANGLES;
 
     this.initGLBuffers();
-    }
-}
+};
--- tp1/primitives/MySphere.js
+++ tp1/primitives/MySphere.js
@@ -1,63 +1,63 @@
-class MySphere{
-    contructor(scene, radius, stacks, slices)
-    {
-        CGFobject.call(this, scene);
+function MySphere(scene, radius, slices, stacks) {
+    CGFobject.call(this, scene);
 
-        this.radius = radius;
-        this.slices = slices;
-        this.stacks = stacks;
+    this.slices = slices;
+    this.stacks = stacks;
+    this.radius = radius;
 
-        this.initBuffers();
-    }
+    this.initBuffers();
+};
 
-    initBuffers() {
+MySphere.prototype = Object.create(CGFobject.prototype);
+MySphere.prototype.constructor = MySphere;
 
-        this.vertices = [];
-        this.normals = [];
-        this.indices = [];
-        this.originalTexCoords = [];
+MySphere.prototype.initBuffers = function() {
 
-        var theta = (2 * Math.PI) / this.slices; // 0-360 deg -- longitude
-        var phi = (Math.PI) / this.stacks; // 0-180 deg -- latitude
-        var n_verts = 0;
+    this.vertices = [];
+    this.normals = [];
+    this.indices = [];
+    this.originalTexCoords = [];
 
-        var patchLengthx = 1 / this.slices;
-        var patchLengthy = 1 / this.stacks;
-        var xCoord = 0;
-        var yCoord = 0;
+    var theta = (2 * Math.PI) / this.slices; // 0-360 deg -- longitude
+    var phi = (Math.PI) / this.stacks; // 0-180 deg -- latitude
+    var n_verts = 0;
 
-
-        for (let i = 0; i <= this.slices; i++) {
-            for (let j = 0; j <= this.stacks; j++) {
-
-                let x = Math.cos(theta * i) * Math.sin(phi * j);
-                let y = Math.sin(theta * i) * Math.sin(phi * j);
-                let z = Math.cos(phi * j);
-
-                this.vertices.push(this.radius * x, this.radius * y, this.radius * z);
-                n_verts++;
-
-                this.normals.push(x, y, z);
-
-                if (i > 0 && j > 0) {
-                    this.indices.push(n_verts - this.stacks - 1, n_verts - 1, n_verts - this.stacks - 2);
-                    this.indices.push(n_verts - 1, n_verts - 2, n_verts - this.stacks - 2);
-                }
+    var patchLengthx = 1 / this.slices;
+    var patchLengthy = 1 / this.stacks;
+    var xCoord = 0;
+    var yCoord = 0;
 
 
-                    yCoord += patchLengthy;
+    for (var i = 0; i <= this.slices; i++) {
+        for (var j = 0; j <= this.stacks; j++) {
 
-              //  this.originalTexCoords.push(0.5 * x + 0.5, 0.5 - 0.5 * y);
-                this.originalTexCoords.push(  xCoord,yCoord);
+            let x = Math.cos(theta * i) * Math.sin(phi * j);
+            let y = Math.sin(theta * i) * Math.sin(phi * j);
+            let z = Math.cos(phi * j);
+
+            this.vertices.push(this.radius * x, this.radius * y, this.radius * z);
+            n_verts++;
+
+            this.normals.push(x, y, z);
+
+            if (i > 0 && j > 0) {
+                this.indices.push(n_verts - this.stacks - 1, n_verts - 1, n_verts - this.stacks - 2);
+                this.indices.push(n_verts - 1, n_verts - 2, n_verts - this.stacks - 2);
             }
 
-            yCoord = 0;
-            xCoord += patchLengthx;
 
+                yCoord += patchLengthy;
+
+          //  this.originalTexCoords.push(0.5 * x + 0.5, 0.5 - 0.5 * y);
+            this.originalTexCoords.push(  xCoord,yCoord);
         }
 
-        this.texCoords = this.originalTexCoords.slice();
-        this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();   
+        yCoord = 0;
+        xCoord += patchLengthx;
+
     }
-}
+
+    this.texCoords = this.originalTexCoords.slice();
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+};
